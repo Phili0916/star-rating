29e9b006f1052468f9999c91b3825fe0e5f7f5e7
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2024-02-14 14:06:37 +0100

    added handle click function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import StarRating from './components/StarRating';
 function App() {
   return (
     <div className="App">
-      <StarRating />
+      <StarRating numOfStars={10}/>
     </div>
   );
 }
--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,9 +1,43 @@
 import './styles.css'
+import { useState } from 'react'
+import {FaStar} from 'react-icons/fa'
 
-const StarRating = () => {
+const StarRating = ({numOfStars = 5}) => {
+
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    const handleClick = (getCurrentIndex) => {
+        setRating(getCurrentIndex)
+    }
+
+    const handleMouseEnter = (getCurrentIndex) => {
+        setHover(getCurrentIndex)
+    }
+
+    const handleMouseLeave = () => {
+        setHover(rating)
+    }
+
     return (
-        <div>
-            Star Rating
+        <div className='starContainer'>
+            {
+                [...Array(numOfStars)].map((_,index) => {
+                    index += 1
+
+                    return(
+                        <FaStar 
+                        key={index}
+                        className={index <= (hover || rating) ? 'active' : 'inactive'}
+                        onClick={()=> handleClick(index)}
+                        onMouseMove={()=> handleMouseEnter(index)}
+                        onMouseLeave={()=> handleMouseLeave()}
+                        size={40}
+                        />
+                    )
+                })
+            }
+            
         </div>
     )
 }
